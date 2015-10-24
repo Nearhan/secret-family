4f2cbd7f3ba9acd57828925b5bc346ad60c7e360
Farhan Syed <nearhan@gmail.com>
2015-10-24 07:58:30 -0400

    wip p# Please enter the commit message for your changes. Lines starting

---
+++ public/javascripts/main.js
@@ -0,0 +1,10 @@
+var React = require('React');
+
+
+var Scene = React.createClass({
+    render: function() {
+        return (<div> hi </div>);
+    }
+});
+
+ReactDOM.render(<Scene  />, document.getElementById('container');
