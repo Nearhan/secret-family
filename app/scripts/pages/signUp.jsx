ebd382ec258c2e6ca9bd6c6ed2f5fb4e8dbb2bed
Farhan Syed <nearhan@gmail.com>
2015-10-24 13:08:30 -0400

    basic stuff:w

---
+++ app/scripts/pages/signUp.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ItemList from '../components/itemList.jsx';
+import ItemStore from '../stores/itemStore';
+import ItemActions from '../actions/itemActions';
+import { Button } from 'react-bootstrap';
+import { Link, Router } from 'react-router';
+
+class Home extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      items : [],
+      loading: false
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = ItemStore.listen(this.onStatusChange.bind(this));
+    ItemActions.loadItems();
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onStatusChange(state) {
+    this.setState(state);
+  }
+
+  click(e) {
+      console.log('hi')
+      console.log(Router)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1> Secret Family New Jersey </h1>
+        <Button bsStyle="primary" bsSize="large" active onClick={this.click}> <Link to="/" />Sign Up</Button>
+      </div>
+    );
+  }
+}
+
+export default Home;
